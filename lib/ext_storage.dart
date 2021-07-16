ecb58d8ed8429727d4205e3c28f383e3a3790367
Ayush Singh <ayush.singh@hyperdart.com>
2021-07-16 08:15:39 +0530

    Added null safety and removed old kotlin version

--- lib/ext_storage.dart
+++ lib/ext_storage.dart
@@ -6,38 +6,38 @@ import 'package:flutter/services.dart';
 class ExtStorage {
   static const MethodChannel _channel = const MethodChannel('ext_storage');
 
-  static final String DIRECTORY_MUSIC = "Music";
+  static const String DIRECTORY_MUSIC = "Music";
 
-  static final String DIRECTORY_PODCASTS = "Podcasts";
+  static const String DIRECTORY_PODCASTS = "Podcasts";
 
-  static final String DIRECTORY_RINGTONES = "Ringtones";
+  static const String DIRECTORY_RINGTONES = "Ringtones";
 
-  static final String DIRECTORY_ALARMS = "Alarms";
+  static const String DIRECTORY_ALARMS = "Alarms";
 
-  static final String DIRECTORY_NOTIFICATIONS = "Notifications";
+  static const String DIRECTORY_NOTIFICATIONS = "Notifications";
 
-  static final String DIRECTORY_PICTURES = "Pictures";
+  static const String DIRECTORY_PICTURES = "Pictures";
 
-  static final String DIRECTORY_MOVIES = "Movies";
+  static const String DIRECTORY_MOVIES = "Movies";
 
-  static final String DIRECTORY_DOWNLOADS = "Download";
+  static const String DIRECTORY_DOWNLOADS = "Download";
 
-  static final String DIRECTORY_DCIM = "DCIM";
+  static const String DIRECTORY_DCIM = "DCIM";
 
-  static final String DIRECTORY_DOCUMENTS = "Documents";
+  static const String DIRECTORY_DOCUMENTS = "Documents";
 
-  static final String DIRECTORY_SCREENSHOTS = "Screenshots";
+  static const String DIRECTORY_SCREENSHOTS = "Screenshots";
 
-  static final String DIRECTORY_AUDIOBOOKS = "Audiobooks";
+  static const String DIRECTORY_AUDIOBOOKS = "Audiobooks";
 
-  static Future<String> getExternalStorageDirectory() async {
+  static Future<String?> getExternalStorageDirectory() async {
     if (!Platform.isAndroid) {
       throw UnsupportedError("Only android supported");
     }
     return await _channel.invokeMethod('getExternalStorageDirectory');
   }
 
-  static Future<String> getExternalStoragePublicDirectory(String type) async {
+  static Future<String?> getExternalStoragePublicDirectory(String type) async {
     if (!Platform.isAndroid) {
       throw UnsupportedError("Only android supported");
     }
